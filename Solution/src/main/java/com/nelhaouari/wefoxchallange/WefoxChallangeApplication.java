665b6bcf4e09254246014de238fdebb1c96198ab
Nabil <n.elhaououari@gmail.com>
2021-07-06 23:35:51 +0100

    Create basic spring boot projet

---
+++ Solution/src/main/java/com/nelhaouari/wefoxchallange/WefoxChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.nelhaouari.wefoxchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefoxChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WefoxChallangeApplication.class, args);
+    }
+
+}
---
+++ Solution/src/test/java/com/nelhaouari/wefoxchallange/WefoxChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelhaouari.wefoxchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefoxChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
